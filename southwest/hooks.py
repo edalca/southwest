c0344dea3e52e1211d8aaf9847871cd4d4eccddd
edalca <edwinalonso162@hotmail.com>
2026-04-24 01:00:31 -0600

    chore(release): bump version to 16.5.5

--- southwest/hooks.py
+++ southwest/hooks.py
@@ -5,7 +5,7 @@ app_publisher = "Edwin Carrillo"
 app_description = "Custom adaptation for Southwest company built on top of ERPNext + HRMS"
 app_email = "edwinalonso162@hotmail.com"
 app_license = "mit"
-app_version = "16.5.4"
+app_version = "16.5.5"
 # Installation hooks
 after_install = "southwest.setup.after_install"
 before_uninstall = "southwest.setup.before_uninstall"
